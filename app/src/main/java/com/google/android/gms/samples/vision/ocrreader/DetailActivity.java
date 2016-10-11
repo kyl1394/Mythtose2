8e8bfd747c15552fb73740f0ac41a69f06fc8fc3
Wesley Vansteenburg <wesley.vansteenburg@gmail.com>
2016-10-11 11:28:52 -0500

    Got APieChart Working

--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/DetailActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/DetailActivity.java
@@ -1,13 +1,107 @@
 package com.google.android.gms.samples.vision.ocrreader;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.SeriesSelection;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 
 public class DetailActivity extends AppCompatActivity {
+
+    /** Colors to be used for the pie slices. */
+    private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
+    /** The main series that will include all the data. */
+    private CategorySeries mSeries = new CategorySeries("");
+    /** The main renderer for the main dataset. */
+    private DefaultRenderer mRenderer = new DefaultRenderer();
+    /** Button for adding entered data to the current series. */
+    private Button mAdd;
+    /** Edit text field for entering the slice value. */
+    private EditText mValue;
+    /** The chart view that displays the data. */
+    private GraphicalView mChartView;
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
+        mSeries = (CategorySeries) savedState.getSerializable("current_series");
+        mRenderer = (DefaultRenderer) savedState.getSerializable("current_renderer");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("current_series", mSeries);
+        outState.putSerializable("current_renderer", mRenderer);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        mRenderer.setZoomButtonsVisible(true);
+        mRenderer.setStartAngle(180);
+        mRenderer.setDisplayValues(true);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null) {
+            /* ** Main code to add to pie chart
+            mSeries.add("Series " + (mSeries.getItemCount() + 1), value);
+            SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
+            renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
+            mRenderer.addSeriesRenderer(renderer);
+            mChartView.repaint();
+             */
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mChartView == null) {
+            LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+            mChartView = ChartFactory.getPieChartView(this, mSeries, mRenderer);
+            mRenderer.setClickEnabled(true);
+            mChartView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
+                    if (seriesSelection == null) {
+                        Toast.makeText(DetailActivity.this, "No chart element selected", Toast.LENGTH_SHORT)
+                                .show();
+                    } else {
+                        for (int i = 0; i < mSeries.getItemCount(); i++) {
+                            mRenderer.getSeriesRendererAt(i).setHighlighted(i == seriesSelection.getPointIndex());
+                        }
+                        mChartView.repaint();
+
+                        /* TODO Add a fragment to show data */
+                        Toast.makeText(
+                                DetailActivity.this,
+                                "Chart data point index " + seriesSelection.getPointIndex() + " selected"
+                                        + " point value=" + seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.FILL_PARENT));
+        } else {
+            mChartView.repaint();
+        }
     }
 }
