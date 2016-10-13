93fdb91e3a505dd509bb7cca10b4175ea98c6635
Kyle Rohlfing <krohlfing94@gmail.com>
2016-10-12 19:41:07 -0500

    Things are working mostly

--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/DetailActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/DetailActivity.java
@@ -1,27 +1,16 @@
 package com.google.android.gms.samples.vision.ocrreader;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.graphics.Color;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
@@ -30,12 +19,13 @@ import org.achartengine.model.SeriesSelection;
 import org.achartengine.renderer.DefaultRenderer;
 import org.achartengine.renderer.SimpleSeriesRenderer;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity  {
 
     /** Colors to be used for the pie slices. */
     private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
@@ -43,27 +33,27 @@ public class DetailActivity extends AppCompatActivity {
     private CategorySeries mSeries = new CategorySeries("");
     /** The main renderer for the main dataset. */
     private DefaultRenderer mRenderer = new DefaultRenderer();
-    /** Button for adding entered data to the current series. */
-    private Button mAdd;
-    /** Edit text field for entering the slice value. */
-    private EditText mValue;
     /** The chart view that displays the data. */
     private GraphicalView mChartView;
-    /** */
+    /** The info that the chart shows. */
     private ArrayList<ChartData> chartInfo;
+    /** Allows information to be added when this activity is first created */
+    private boolean onStart = false;
 
     @Override
     protected void onRestoreInstanceState(Bundle savedState) {
         super.onRestoreInstanceState(savedState);
-        //mSeries = (CategorySeries) savedState.getSerializable("current_series");
-        //mRenderer = (DefaultRenderer) savedState.getSerializable("current_renderer");
+        mSeries = (CategorySeries) savedState.getSerializable("current_series");
+        mRenderer = (DefaultRenderer) savedState.getSerializable("current_renderer");
+        chartInfo = (ArrayList<ChartData>) savedState.getSerializable("current_data");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //outState.putSerializable("current_series", mSeries);
-        //outState.putSerializable("current_renderer", mRenderer);
+        outState.putSerializable("current_series", mSeries);
+        outState.putSerializable("current_renderer", mRenderer);
+        outState.putSerializable("current_data", chartInfo);
     }
 
     @Override
@@ -79,61 +69,62 @@ public class DetailActivity extends AppCompatActivity {
         mRenderer.setStartAngle(180);
         mRenderer.setDisplayValues(true);
 
-        Set<Ingredient> ingreds = matchIngredientsToDatabase();
-        HashMap<String, Integer> categories = getNumCategories(ingreds);
+        Bundle extras = getIntent().getExtras();
 
-        if(categories.size() != 0) {
-            /* ** Main code to add to pie chart */
+        /**************************
+         TODO: fill the array list with real data instead of this junk
+
+         */
+
+        if(extras != null) {
+            chartInfo = new ArrayList<>();
+            HashMap<String, Integer> categories = (HashMap<String, Integer>) extras.get("categories");
             Set<String> types = categories.keySet();
             Iterator<String> iter = types.iterator();
             while (iter.hasNext()) {
-                String name = iter.next();
-                mSeries.add(name, categories.get(name));
-                SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
-                renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
-                mRenderer.addSeriesRenderer(renderer);
+                String categoryName = iter.next();
+                ArrayList<String> ingredients = new ArrayList<>();
+                for (int i = 0; i < MainActivity.ingredientDatabase.size(); i++) {
+                    if (MainActivity.ingredientDatabase.get(i).type.equals(categoryName)) {
+                        ingredients.add(MainActivity.ingredientDatabase.get(i).name);
+                    }
+                }
+                chartInfo.add(new ChartData(categoryName, categories.get(categoryName), ingredients));
             }
 
-            updateChart();
+            onStart = true;
         }
 
         if(chartInfo == null) {
             chartInfo = new ArrayList<>();
+
             for (int x = 0; x < 10; x++) {
-                ArrayList<String> links = new ArrayList<String>();
-                links.add("<a href=\"google.com\">Click Google! " + x + "</a>");
-                links.add("<a href=\"facebook.com\">Click Facebook! +" + x + "</a>");
+                ArrayList<String> links = new ArrayList<>();
+                links.add("Click Google! " + x);
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+                links.add("Click Facebook! " + x );
+
+
                 chartInfo.add(new ChartData("Element " + (char) ('A' + x), x, links));
             }
+            onStart = true;
         }
-    }
-
-    private HashMap<String,Integer> getNumCategories(Set<Ingredient> ingreds) {
-        Iterator<Ingredient> ingredIter = ingreds.iterator();
-        HashMap<String, Integer> toReturn = new HashMap<>();
-
-        while (ingredIter.hasNext()) {
-            Ingredient ingred = ingredIter.next();
-            int add = -1;
-            if (toReturn.containsKey(ingred.type)) {
-                add = toReturn.get(ingred.type) + 1;
-            } else {
-                add = 1;
-            }
-
-            toReturn.put(ingred.type, add);
-        }
-
-        return toReturn;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateChart();
-    }
-
-    private void updateChart() {
         if (mChartView == null) {
             LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
             mChartView = ChartFactory.getPieChartView(this, mSeries, mRenderer);
@@ -151,192 +142,54 @@ public class DetailActivity extends AppCompatActivity {
                         }
                         mChartView.repaint();
 
-                        /* TODO Add an intent */
+                        /* Old unused code to determine the selection
                         Toast.makeText(
                                 DetailActivity.this,
                                 "Chart data point index " + seriesSelection.getPointIndex() + " selected"
                                         + " point value=" + seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
+                        */
+                        //Update Fragment
+                        PopupFragment frag = (PopupFragment) getFragmentManager().findFragmentById(R.id.mainActivityFragment);
 
-                        //Remvove old Fragment
-                        Fragment frag = getFragmentManager().findFragmentByTag("main fragment");
-                        //NULL POINTER EXCEPTION ON FRAG
-                        getFragmentManager().beginTransaction().remove(frag).commit();
-
-
-                        //Attach new Fragment
-
-                        Bundle infoBundle = new Bundle();
-                        frag = new PopupFragment();
-                        infoBundle.putStringArrayList("array", chartInfo.get(seriesSelection.getPointIndex()).links);
-                        frag.setArguments(infoBundle);
-                        getFragmentManager().beginTransaction().replace(R.id.mainActivityFragment, frag, "main fragment").commit();
-
+                        frag.update(chartInfo.get(seriesSelection.getPointIndex()).links);
                     }
                 }
             });
-            layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
-                    ViewGroup.LayoutParams.FILL_PARENT));
+            layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
         } else {
             mChartView.repaint();
         }
 
-//        for(ChartData cd: chartInfo) {
-//            mSeries.add(cd.name, cd.amount);
-//            SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
-//            renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
-//            mRenderer.addSeriesRenderer(renderer);
-//
-//            mChartView.repaint();
-//        }
-    }
+        if(onStart) {
+            Log.d("DetailActivity", "Reloaded Data");
+            for (ChartData cd : chartInfo) {
+                mSeries.add(cd.name, cd.amount);
+                SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
+                renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
+                mRenderer.addSeriesRenderer(renderer);
 
-    protected class ChartData {
-        public String name;
-
-        public int amount;
-
-        public ArrayList<String> links;
-
-        public ChartData() {
-            this("", 0, new ArrayList<String>());
-        }
-
-        public ChartData(String chartName, int chartAmount, ArrayList<String> chartLinks) {
-            name = chartName;
-            amount = chartAmount;
-            links = chartLinks;
-        }
-    }
-
-    private Set<Ingredient> matchIngredientsToDatabase() {
-        ArrayList<Ingredient> ingredientDatabase = MainActivity.ingredientDatabase;
-
-        HashMap<Ingredient, Double> potentialIngredients = new HashMap<>();
-        ArrayList<String> finalIngredients = new ArrayList<>();
-
-        String text = null;
-        String testString = "";
-
-        if (OcrDetectorProcessor.candidates != null) {
-            ArrayList<String> candidates = OcrDetectorProcessor.candidates;
-            ArrayList<String> spelledCorrectly = new ArrayList<>();
-            ArrayList<String> needsCorrected = new ArrayList<>();
-
-            for (int candidateIndex = 0; candidateIndex < candidates.size(); candidateIndex++) {
-                String[] candidate = candidates.get(candidateIndex).split(" ");
-
-                for (int candidateIngredientIndex = 0; candidateIngredientIndex < candidate.length; candidateIngredientIndex++) {
-                    double maxSimilarity = -1;
-                    Ingredient mostSimilar = null;
-                    int numWordsAdded = 0;
-                    for (int wordIndex = 0; wordIndex < ingredientDatabase.size(); wordIndex++) {
-                        double temp = similarity(candidate[candidateIngredientIndex], ingredientDatabase.get(wordIndex).name);
-                        double addNextWord = 0;
-                        numWordsAdded = 0;
-
-                        for (int i = candidateIngredientIndex; i < candidate.length; i++) {
-                            String multiWordIngredient = candidate[candidateIngredientIndex];
-                            multiWordIngredient += " " + candidate[i];
-
-                            addNextWord = similarity(multiWordIngredient, ingredientDatabase.get((wordIndex)).name);
-                            if (addNextWord > temp) {
-                                temp = addNextWord;
-                                numWordsAdded++;
-                            } else if (addNextWord < temp) {
-                                break;
-                            }
-                        }
-
-                        if (temp > maxSimilarity) {
-                            maxSimilarity = temp;
-                            mostSimilar = ingredientDatabase.get(wordIndex);
-                        }
-                    }
-
-                    if ((!potentialIngredients.containsKey(mostSimilar) || potentialIngredients.get(mostSimilar) < maxSimilarity) && maxSimilarity > 0.45) {
-                        potentialIngredients.put(mostSimilar, maxSimilarity);
-                    }
-
-                    candidateIngredientIndex += numWordsAdded;
-                }
+                mChartView.repaint();
             }
-
-            return potentialIngredients.keySet();
+            onStart = false;
         }
+    }
+}
 
-        return null;
+class ChartData implements Serializable {
+    public String name;
+
+    public int amount;
+
+    public ArrayList<String> links;
+
+    public ChartData() {
+        this("", 0, new ArrayList<String>());
     }
 
-    boolean finished = false;
-    boolean reached = false;
-    private ArrayList<String> getIngredientsFromDatabase() {
-        DatabaseReference fbDbRef = FirebaseDatabase.getInstance().getReference("0");
-        final ArrayList<String> ingredientList = new ArrayList<>();
-        finished = false;
-
-        fbDbRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                reached = true;
-                Iterator iter = snapshot.getChildren().iterator();
-                while (iter.hasNext()) {
-                    DataSnapshot snap = (DataSnapshot) iter.next();
-                    ingredientList.add(snap.getKey());
-                }
-                finished = true;
-            }
-
-            @Override
-            public void onCancelled(DatabaseError firebaseError) {
-                System.out.println("The read failed: " + firebaseError.getMessage());
-                return;
-            }
-        });
-
-        while (!finished) {
-            // You must give this loop something to do otherwise running it out of debug mode will just cause it to hang
-            // for some unknown reason...
-            System.out.println("loading...");
-        }
-
-        finished = false;
-        return ingredientList;
-    }
-
-    private static double similarity(String s1, String s2) {
-        String longer = s1, shorter = s2;
-        if (s1.length() < s2.length()) { // longer should always have greater length
-            longer = s2; shorter = s1;
-        }
-        int longerLength = longer.length();
-        if (longerLength == 0) { return 1.0; /* both strings are zero length */ }
-        return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
-    }
-
-    private static int editDistance(String s1, String s2) {
-        s1 = s1.toLowerCase();
-        s2 = s2.toLowerCase();
-
-        int[] costs = new int[s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            int lastValue = i;
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0)
-                    costs[j] = j;
-                else {
-                    if (j > 0) {
-                        int newValue = costs[j - 1];
-                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                            newValue = Math.min(Math.min(newValue, lastValue),
-                                    costs[j]) + 1;
-                        costs[j - 1] = lastValue;
-                        lastValue = newValue;
-                    }
-                }
-            }
-            if (i > 0)
-                costs[s2.length()] = lastValue;
-        }
-        return costs[s2.length()];
+    public ChartData(String chartName, int chartAmount, ArrayList<String> chartLinks) {
+        name = chartName;
+        amount = chartAmount;
+        links = chartLinks;
     }
 }
--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/PopupFragment.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/PopupFragment.java
@@ -2,43 +2,41 @@ package com.google.android.gms.samples.vision.ocrreader;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
+ * This is the fragment used to display the information that each part of the pie chart will show.
  * Created by Wes on 10/11/2016.
  */
 
 public class PopupFragment extends Fragment {
-    private Activity mParent = null;
+
+    public static final String LINKS_ARGUMENT = "array";
+
+    private Context mParent = null;
     private ArrayList<String> links;
-    private ListView list;
+    private LinksAdapter adapter;
 
     @Override
-    public void onAttach(Activity parent) {
+    public void onAttach(Context parent) {
         super.onAttach(parent);
         mParent = parent;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState) {
-
-        Log.w("PopupFragment", "Creating..." + savedInstanceState);
-
         Bundle arguments = this.getArguments();
 
         if(arguments != null) {
-            links = arguments.getStringArrayList("array");
+            links = arguments.getStringArrayList(LINKS_ARGUMENT);
         }
 
         return inflator.inflate(R.layout.popup_fragment, container, false);
@@ -47,14 +45,24 @@ public class PopupFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        list = (ListView) getView().findViewById(R.id.linksList);
+        ListView list = (ListView) getView().findViewById(R.id.linksList);
 
-        if(links == null) {
-            Log.e("PopupFragment", "Links ArrayList was null. Error in sending data");
+        if(links == null || links.size() == 0) {
+            Log.e("PopupFragment", "Links ArrayList was empty.");
             links = new ArrayList<>();
         }
+        adapter = new LinksAdapter(mParent, android.R.layout.simple_list_item_1, links);
+        list.setAdapter(adapter);
+    }
 
-        list.setAdapter(new LinksAdapter(mParent, android.R.layout.simple_list_item_1, links));
+    public void update(ArrayList<String> newList) {
+        links.clear();
+        links.addAll(newList);
+        Log.d("New List size", ""+newList.size());
+
+//        list.setAdapter(new LinksAdapter(mParent, android.R.layout.simple_list_item_1, links));
+        adapter.notifyDataSetChanged();
+
     }
 
 }
